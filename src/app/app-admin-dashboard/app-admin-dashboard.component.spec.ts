cdc8a65f4ee2aac47f9cec1520c8efae26c9c47e
aolite <aitorcorchero@gmail.com>
2017-11-09 01:06:52 +0100

    add dashboard component

---
+++ src/app/app-admin-dashboard/app-admin-dashboard.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppAdminDashboardComponent } from './app-admin-dashboard.component';
+
+describe('AppAdminDashboardComponent', () => {
+  let component: AppAdminDashboardComponent;
+  let fixture: ComponentFixture<AppAdminDashboardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AppAdminDashboardComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppAdminDashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/app-admin-dashboard/app-admin-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-admin-dashboard',
+  templateUrl: './app-admin-dashboard.component.html',
+  styleUrls: ['./app-admin-dashboard.component.css']
+})
+export class AppAdminDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
